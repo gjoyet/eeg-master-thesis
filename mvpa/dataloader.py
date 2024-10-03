de22dc17ad131f5fd478d176e2b83b09ac46c514
Guillaume Joyet <guillaume.joyet@unibas.ch>
2024-10-03 13:29:36 +0200

    commit before changing scaling method.
    now: StandardScaler applied on each experiment block separately, scales channels over all time-points of all trials of a block.

--- mvpa/dataloader.py
+++ mvpa/dataloader.py
@@ -25,16 +25,18 @@ behav_data_path = '/Volumes/Guillaume EEG Project/Berlin_Data/EEG/raw'
 
 '''
 TODOS:
-    - Most pressing issue: channels only vary at very small scales. 
+    - Most pressing issue: channels only vary at very small scales inside a block, but a lot between blocks. 
         How do still make an SVM work? Scaling? Can PCA also help?
         -> I need to scale each block individually I think. Therefore do not concatenate epochs and scale them
            at the beginning of decode_subject_response_over_time().
+        -> Better for now, but maybe I should even scale each channel separately?
+        -> TO TEST:  Scaler w/o PCA (now), PCA w/o Scaler, Scaler then PCA, PCA then Scaler
+        -> OR TEST: normalise each channel for each trial by first subtracting the mean of the activation from
+                    t = -1000ms to t = 0ms, then scale.
 
-    - Debug problem with discrepant bad channels.
+    - Debug problem with discrepant bad channels and issues with labels from files with unclear names of result files.
 
     - COMMENT this code early enough!
-    - Cross-validation (or generally validate SVM accuracy on a part of the data it has not seen during training).
-    - PCA? ... ?
     
     - Frequency analysis?
 '''
@@ -46,8 +48,9 @@ def calculate_mean_decoding_accuracy():
     accuracies = []
 
     for subject_id in subject_ids:
-        epochs, labels = load_subject_train_data(subject_id)
-        acc = decode_subject_response_over_time(epochs, labels)
+        print("\n------------------------\nLOGGER: Decoding subject #{}\n------------------------\n".format(subject_id))
+        epochs_list, labels_list = load_subject_train_data(subject_id)
+        acc = decode_subject_response_over_time(epochs_list, labels_list)
         accuracies.append(acc)
 
     accuracies = np.array(accuracies)
@@ -88,37 +91,47 @@ def load_results_and_plot(path: str) -> None:
     plt.show()
 
 
-def decode_subject_response_over_time(epochs: EpochsFIF, labels: List[int]) -> List[float]:
-    data = epochs.get_data()
+def decode_subject_response_over_time(epochs_list: List[EpochsFIF], labels_list: List[List[int]]) -> List[float]:
+    scaled_epochs = []
+    labels_filtered = []
 
-    data_filtered = data[~np.isnan(labels)]
-    labels_filtered = labels[~np.isnan(labels)]
+    # This loop rescales the data one experiment block at a time. This is because baseline channel activation
+    # varies a lot between blocks, but channel activation varies on a much smaller scale inside a block.
+    for epoch, labels in zip(epochs_list, labels_list):
+        data = epoch.get_data()
 
-    scaler = StandardScaler()
-    num_epochs, num_channels, num_timesteps = data_filtered.shape
+        data_filtered = data[~np.isnan(labels)]
+        labels_filtered.extend(labels[~np.isnan(labels)])
 
-    # Transpose and reshape for StandardScaler (only takes 2d input)
-    data_filtered = np.transpose(data_filtered, axes=(0, 2, 1))
-    data_filtered = np.reshape(data_filtered, shape=(-1, num_channels))
+        scaler = StandardScaler()
+        num_epochs, num_channels, num_timesteps = data_filtered.shape
 
-    data_scaled = scaler.fit_transform(data_filtered)
+        # Transpose and reshape for StandardScaler (only takes 2d input)
+        data_filtered = np.transpose(data_filtered, axes=(0, 2, 1))
+        data_filtered = np.reshape(data_filtered, shape=(-1, num_channels))
 
-    # Reshape and transpose back
-    data_scaled = np.reshape(data_scaled, shape=(num_epochs, num_timesteps, num_channels))
-    data_scaled = np.transpose(data_scaled, axes=(0, 2, 1))
+        data_scaled = scaler.fit_transform(data_filtered)
 
+        # Reshape and transpose back
+        data_scaled = np.reshape(data_scaled, shape=(num_epochs, num_timesteps, num_channels))
+        data_scaled = np.transpose(data_scaled, axes=(0, 2, 1))
+
+        scaled_epochs.append(data_scaled)
+
+    scaled_epochs = np.concat(scaled_epochs, axis=0)
+    labels_filtered = np.array(labels_filtered)
     accuracies = []
 
-    for t in range(len(epochs.times)):
+    for t in range(scaled_epochs.shape[-1]):
         # clf = make_pipeline(StandardScaler(), LinearSVC(C=1.0, random_state=0, tol=1e-5))
         clf = svm.SVC(kernel='linear', C=1, random_state=42)
-        scores = cross_val_score(clf, data_scaled[:, :, t], labels_filtered, cv=5)
+        scores = cross_val_score(clf, scaled_epochs[:, :, t], labels_filtered, cv=5)
         accuracies.append(np.mean(scores))
 
     return accuracies
 
 
-def load_subject_train_data(subject_id: int) -> Tuple[EpochsFIF, List[int]]:
+def load_subject_train_data(subject_id: int) -> Tuple[List[EpochsFIF], List[np.ndarray[int]]]:
     epochs_dict = load_subject_epochs(subject_id)
     results_df = load_subject_labels(subject_id)
 
@@ -129,16 +142,12 @@ def load_subject_train_data(subject_id: int) -> Tuple[EpochsFIF, List[int]]:
         epochs = epochs_dict[block]
         labels = (results_df[results_df['run'] == block]['response']).reset_index(drop=True)
 
-        print('Next subject: {}'.format(subject_id))
-
         selected_labels = labels.loc[epochs.selection]
 
         epochs_list.append(epochs)
-        labels_list.extend(selected_labels)
+        labels_list.append(np.array(selected_labels))
 
-        concatenated_epochs = mne.concatenate_epochs(epochs_list)
-
-    return concatenated_epochs, np.array(labels_list)
+    return epochs_list, labels_list
 
 
 def load_subject_epochs(subject_id: int) -> Dict[int, EpochsFIF]:
