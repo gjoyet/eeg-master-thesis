0d74654bf8db0adc0d35abd3315f99040e851957
Guillaume Joyet <guillaume.joyet@unibas.ch>
2024-10-11 11:04:05 +0200

    Make PCA optional. Make scaling to be computed over all blocks instead of each block individually. Reformatting and commenting.

--- mvpa/dataloader.py
+++ mvpa/dataloader.py
@@ -1,6 +1,6 @@
 import os.path
 import re
-from typing import Dict, Tuple, List, Callable
+from typing import Dict, Tuple
 
 import mne
 import numpy as np
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 from mne.epochs import EpochsFIF
+
 
 epoched_data_path = '/Volumes/Guillaume EEG Project/Berlin_Data/EEG/preprocessed/stim_epochs'
 behav_data_path = '/Volumes/Guillaume EEG Project/Berlin_Data/EEG/raw'
@@ -46,7 +47,7 @@ def average_augment_data(epochs: np.ndarray[float],
             partitions = np.array_split(shuffled_array, n)
             partition_averages = np.stack([np.mean(p, axis=0) for p in partitions])
 
-            # Step 3: Take the mean over dimension 1 (e.g. epochs inside a partition)
+            # Step 3: Take the mean over epochs in each partition
             pseudo_epochs.append(partition_averages)
             pseudo_labels.extend(np.full(partition_averages.shape[0], lab))  # TODO: check extend works with np.ndarray
 
@@ -61,84 +62,64 @@ def average_augment_data(epochs: np.ndarray[float],
     return pseudo_epochs, pseudo_labels
 
 
-def preprocess_train_data(epochs_list: List[np.ndarray[float]], subsampling_rate: int = 1) -> np.ndarray[float]:
+def preprocess_train_data(data: np.ndarray[float], 
+                          downsample_factor: int = 1, 
+                          perform_PCA: bool = False) -> np.ndarray[float]:
     """
     Preprocesses training data.
-    Scales channels (each block independently),
-    performs PCA retaining 99% of variance (over all epochs).
-    :param epochs_list: list of numpy arrays containing epoch data.
-             Each array in the list has shape #epochs x #channels x #timesteps.
-    :param subsampling_rate: number of samples that are collapsed into one by averaging.
-    :return: numpy array of concatenated processed epoch data (of shape #epochs x #pca_components x #timesteps).
+    Downsamples, scales channels, optionally performs PCA retaining 99% of variance.
+    :param data: numpy arrays containing epoch data (of shape #epochs x #channels x #timesteps).
+    :param downsample_factor: number of samples that are collapsed into one by averaging.
+    :param perform_PCA: if True, PCA is performed on the data.
+    :return: numpy array of processed epoch data (of shape #epochs x #channels x #timesteps).
     """
     '''
     For now things like scaling and PCA are calculated on complete data.
-    If we want to calculate it on train and apply it on test, we will need to do it differently
-    (since for now train/test splits are done inside cross_val_scores).
+    If we want to calculate it on train and only apply it on test, we will need to do it differently
+    (since for now train/test splits are computed inside cross_val_scores).
     -> I could just do my own cross-validation, as the only thing it requires is calculating the
        indices for the folds (something like random_partition(range()) probably exists) and then
        run it once for each fold.
     '''
-    proc_epochs = []
 
-    # This loop rescales the data one experiment block at a time. This is because baseline channel activation
-    # varies a lot between blocks, but channel activation varies on a much smaller scale inside a block.
-    for data in epochs_list:
-        num_epochs, num_channels, num_timesteps = data.shape
-
-        # SUBSAMPLE from 1000 Hz to (1000 / subsampling_rate) Hz
-        # Ignore first time-step since there are 2251 time-steps, which is not easily divisible.
-        if subsampling_rate > 1:
-            data = np.reshape(data[:, :, 1:], shape=(num_epochs, num_channels, num_timesteps // subsampling_rate,
-                                                    subsampling_rate))
-            data = np.mean(data, axis=-1)
-
-        # SCALE
-        scaler = StandardScaler()
-        data = reshape_transform_reshape(data=data, func=scaler.fit_transform)
-
-        proc_epochs.append(data)
-
-    proc_epochs = np.concat(proc_epochs, axis=0)
-
-    # PERFORM PCA (on whole data)
-    pca = PCA(n_components=0.99, svd_solver='full')  # switching svd_solver to 'auto' might increase performance
-    proc_epochs = reshape_transform_reshape(data=proc_epochs, func=pca.fit_transform)
-
-    return proc_epochs
-
-
-def reshape_transform_reshape(data: np.ndarray[float], func: Callable) -> np.ndarray:
-    """
-    Transposes and reshapes data such that features are at the last dimension, performs a transformation,
-    then reshapes and transposes back.
-    :param data: the data to be transformed.
-    :param func: the transformation to be applied.
-    :return: the transformed data.
-    """
     num_epochs, num_channels, num_timesteps = data.shape
 
-    # Transpose and reshape (functions expect features (channels) at last dimension)
+    # DOWNSAMPLE from 1000 Hz to (1000 / downsample_factor) Hz
+    # Ignore first time-step since there are 2251 time-steps, which is not easily divisible.
+    if downsample_factor > 1:
+        data = np.reshape(data[:, :, 1:], shape=(num_epochs, num_channels, num_timesteps // downsample_factor,
+                                                 downsample_factor))
+        data = np.mean(data, axis=-1)
+
+    num_timesteps = data.shape[-1]
+
+    # Transpose and reshape (scaler and PCA expect features (channels) at last dimension)
     data = np.transpose(data, axes=(0, 2, 1))
     data = np.reshape(data, shape=(num_epochs * num_timesteps, num_channels))
 
-    # Perform transformation
-    data = func(data)
+    # SCALE
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
+    
+    if perform_PCA:
+        # PERFORM PCA
+        pca = PCA(n_components=0.99, svd_solver='full')  # switching svd_solver to 'auto' might increase performance
+        data = pca.fit_transform(data)
 
     # Transpose back
     data = np.reshape(data, shape=(num_epochs, num_timesteps, -1))
     data = np.transpose(data, axes=(0, 2, 1))
-
+    
     return data
 
 
-def load_subject_train_data(subject_id: int) -> Tuple[List[np.ndarray[float]], List[np.ndarray[int]]]:
+def load_subject_train_data(subject_id: int) -> Tuple[np.ndarray[float], np.ndarray[int]]:
     """
     Loads and correctly combines epoch data and labels (behavioural outcomes) for one subject.
     Applies baseline and drops NaN labels.
     :param subject_id:
-    :return: list of numpy arrays containing epoch data (one per experiment block),
-             list of numpy arrays with corresponding labels.
+    :return: numpy array containing epoch data (of shape #epochs x #channels x #timesteps),
+             numpy array with corresponding labels (of length #epochs).
     """
     epochs_dict = load_subject_epochs(subject_id)
     results_df = load_subject_labels(subject_id)
@@ -156,18 +137,21 @@ def load_subject_train_data(subject_id: int) -> Tuple[List[np.ndarray[float]], L
 
         data = epochs.get_data()
 
-        # drop NaN labels and corresponding epochs
+        # drop NaN labels (no response) and corresponding epochs
         epochs_list.append(data[~np.isnan(selected_labels)])
         labels_list.append(selected_labels[~np.isnan(selected_labels)])
 
-    return epochs_list, labels_list
+    epochs = np.concat(epochs_list, axis=0)
+    labels = np.concat(labels_list, axis=0)
+
+    return epochs, labels
 
 
 def load_subject_epochs(subject_id: int) -> Dict[int, EpochsFIF]:
     """
     Loads epoch data for one subject.
     :param subject_id:
-    :return: dict, keys are experiment block numbers, values are a mne.Epoch object for the corresponding block.
+    :return: dict, keys are experiment block numbers, values are a mne.EpochsFIF object for the corresponding block.
     """
     filenames = os.listdir(epoched_data_path)
 
@@ -230,7 +214,7 @@ def get_subject_ids(path: str) -> np.ndarray:
     subject_ids = list(set(subject_ids))
     subject_ids.sort()
 
-    # The removals below are just to allow testing: the issues need to be fixed/understood.
+    # The removals below are just to allow testing: the issues need to be fixed.
 
     # TODO: check what is up with those result files (several blocks with same number)
     subject_ids.remove(9)
--- mvpa/decoder.py
+++ mvpa/decoder.py
@@ -1,14 +1,10 @@
 import argparse
-import os.path
 
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
-from datetime import datetime
-import time
-import timeit
 
 import matplotlib
 
@@ -17,13 +13,10 @@ import matplotlib.pyplot as plt
 
 from dataloader import get_subject_ids, load_subject_train_data, preprocess_train_data, average_augment_data
 
+
 epoched_data_path = '/Volumes/Guillaume EEG Project/Berlin_Data/EEG/preprocessed/stim_epochs'
 behav_data_path = '/Volumes/Guillaume EEG Project/Berlin_Data/EEG/raw'
 
-# Get date and time for naming files (prevents overwriting)
-now = datetime.now()
-DATE_TIME = now.strftime("D%Y-%m-%d_T%H-%M-%S")
-print("\n{}\n".format(DATE_TIME))
 
 '''
 TODOS:
@@ -49,7 +42,8 @@ def calculate_mean_decoding_accuracy():
     :return: None
     """
     test = args.test
-    subsampling_rate = args.subsampling_rate
+    downsample_factor = args.downsample_factor
+    perform_PCA = args.perform_PCA
     pseudo_k = args.pseudo_k
     augment_factor = args.augment_factor
     C = args.SVM_C
@@ -57,7 +51,7 @@ def calculate_mean_decoding_accuracy():
 
     print("\n-----------------------------------------------------------------------\n",
           "LOGGER: STARTING RUN WITH PARAMETERS:\n",
-          "{} Hz / {}-fold average / {}-fold augment / SVM: C = {}, window = {}\n".format(1000/subsampling_rate,
+          "{} Hz / {}-fold average / {}-fold augment / SVM: C = {}, window = {}\n".format(1000 / downsample_factor,
                                                                                           pseudo_k,
                                                                                           augment_factor,
                                                                                           C,
@@ -72,33 +66,34 @@ def calculate_mean_decoding_accuracy():
 
     accuracies = []
 
+    # Loop loads, processes and decodes data one subject at a time
     for subject_id in subject_ids:
-        epochs_list, labels_list = load_subject_train_data(subject_id)
-        proc_epochs = preprocess_train_data(epochs_list, subsampling_rate=subsampling_rate)
-        proc_labels = np.concat(labels_list, axis=0)
+        epochs, labels = load_subject_train_data(subject_id)
+        proc_epochs = preprocess_train_data(epochs, downsample_factor=downsample_factor, perform_PCA=perform_PCA)
 
         # TODO: debug this. Does it make sense to do this after PCA? (probably not)
         if pseudo_k > 1:
-            proc_epochs, proc_labels = average_augment_data(proc_epochs, proc_labels,
-                                                            pseudo_k=pseudo_k,
-                                                            augment_factor=augment_factor)
+            proc_epochs, labels = average_augment_data(proc_epochs, labels,
+                                                       pseudo_k=pseudo_k,
+                                                       augment_factor=augment_factor)
 
-        print("\n-----------------------------\nLOGGER: Decoding subject #{:03d}\n".format(subject_id),
+        print("\n-----------------------------\n",
+              "LOGGER: Decoding subject #{:03d}\n".format(subject_id),
               "-----------------------------\n", sep="")
-        acc = decode_subject_response_over_time(proc_epochs, proc_labels, C=C, window_width=window_width)
+        acc = decode_subject_response_over_time(proc_epochs, labels, C=C, window_width=window_width)
 
         accuracies.append(acc)
 
     accuracies = np.array(accuracies)
 
     if not test:
-        np.save('results/data/mvpa_acc_{}Hz_av-{}_aug-{}_C-{}_win-{}.png'.format(int(1000/subsampling_rate),
+        np.save('results/data/mvpa_acc_{}Hz_av-{}_aug-{}_C-{}_win-{}.png'.format(int(1000 / downsample_factor),
                                                                                  pseudo_k,
                                                                                  augment_factor,
-                                                                                 int(C*1000),
+                                                                                 int(C * 1000),
                                                                                  window_width), accuracies)
 
-    plot_accuracies(data=accuracies, subsampling_rate=subsampling_rate, pseudo_k=pseudo_k,
+    plot_accuracies(data=accuracies, downsample_factor=downsample_factor, pseudo_k=pseudo_k,
                     augment_factor=augment_factor, C=C, window_width=window_width)
 
 
@@ -118,23 +113,24 @@ def decode_subject_response_over_time(proc_epochs: np.ndarray[float],
 
     num_epochs = proc_epochs.shape[0]
 
+    # Loop trains SVM for each timestep / window in the epoched data
     for t in range(proc_epochs.shape[-1] - window_width + 1):
         clf = svm.SVC(kernel='linear', C=C, class_weight='balanced')
-        scores = cross_val_score(clf, np.reshape(proc_epochs[:, :, t:t+window_width], shape=(num_epochs, -1)),
+        scores = cross_val_score(clf, np.reshape(proc_epochs[:, :, t:t + window_width], shape=(num_epochs, -1)),
                                  proc_labels, cv=5)
         subject_accuracies.append(np.mean(scores))
 
     return np.array(subject_accuracies)
 
 
-def plot_accuracies(data: np.ndarray = None, path: str = None, subsampling_rate: int = 5,
+def plot_accuracies(data: np.ndarray = None, path: str = None, downsample_factor: int = 5,
                     pseudo_k: int = 4, augment_factor: int = 1, C: int = 1, window_width: int = 1) -> None:
     """
     Plots the mean accuracy over time with confidence band over subjects.
     :param data: 2D numpy array, where each row is the decoding accuracy for one subject over all timesteps.
     :param path: if data is None, this path indicates what file to load the data from.
     The parameters below are hyperparameters of the model passed to this method for naming the plot.
-    :param subsampling_rate:
+    :param downsample_factor:
     :param pseudo_k:
     :param augment_factor:
     :param C:
@@ -152,22 +148,23 @@ def plot_accuracies(data: np.ndarray = None, path: str = None, subsampling_rate:
     plt.figure(figsize=(10, 6))  # Optional: Set the figure size
 
     # Create the lineplot, seaborn will automatically calculate confidence intervals
-    sns.lineplot(data=df, x='Time', y='Mean_Accuracy', errorbar='sd')
+    sns.lineplot(data=df, x=df['Time'] * downsample_factor - 1000, y='Mean_Accuracy', errorbar='sd')
 
     # Set plot labels and title
     plt.xlabel('Time (samples)')
     plt.ylabel('Mean Accuracy')
-    plt.title('{} Hz / {}-fold average / {}-fold augment / SVM: C = {}, window = {}'.format(1000/subsampling_rate,
-                                                                                            pseudo_k,
-                                                                                            augment_factor,
-                                                                                            C,
-                                                                                            window_width))
+    plt.title(
+        '{} Hz / {}-fold average / {}-fold augment / SVM: C = {}, window = {}'.format(int(1000 / downsample_factor),
+                                                                                      pseudo_k,
+                                                                                      augment_factor,
+                                                                                      C,
+                                                                                      window_width))
 
     if path is None:
-        plt.savefig('results/mean_acc_{}Hz_av-{}_aug-{}_C-{}_win-{}.png'.format(int(1000/subsampling_rate),
+        plt.savefig('results/mean_acc_{}Hz_av-{}_aug-{}_C-{}_win-{}.png'.format(int(1000 / downsample_factor),
                                                                                 pseudo_k,
                                                                                 augment_factor,
-                                                                                int(C*1000),
+                                                                                int(C * 1000),
                                                                                 window_width))
 
     # Show the plot
@@ -181,37 +178,42 @@ if __name__ == '__main__':
 
     parser.add_argument('--test',
                         action='store_true',
-                        help='Activate test mode')
+                        help='Activate test mode (only few subjects are decoded for shorter runtime).')
 
-    parser.add_argument('--subsampling_rate',
+    parser.add_argument('--downsample_factor',
                         default=5,
                         type=int,
-                        required=False)
+                        required=False,
+                        help='Number of timesteps that are collapsed into one when downsampling.')
+
+    parser.add_argument('--perform_PCA',
+                        action='store_true',
+                        help='Perform PCA before decoding')
 
     parser.add_argument('--pseudo_k',
                         default=1,
                         type=int,
-                        required=False)
+                        required=False,
+                        help='Number of trials that are averaged. If 1, no trial averaging is performed.')
 
     parser.add_argument('--augment_factor',
                         default=1,
                         type=int,
-                        required=False)
+                        required=False,
+                        help='Factor with which the data is augmented (only matters when pseudo_k > 1).')
 
     parser.add_argument('--SVM_C',
                         default=1,
                         type=float,
-                        required=False)
+                        required=False,
+                        help='Regularisation parameter of the SVM.')
 
     parser.add_argument('--SVM_window_width',
                         default=1,
                         type=int,
-                        required=False)
+                        required=False,
+                        help='Width of time window that is fed as input to the SVM.')
 
     args = parser.parse_args()
 
     calculate_mean_decoding_accuracy()
-
-    # plot_accuracies(path='/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master Thesis/Code/eeg-master-thesis/mvpa/results/data/mvpa_accuracies_D2024-10-07_T14-26-14.npy')
-    # plot_accuracies(path='/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master Thesis/Code/eeg-master-thesis/mvpa/results/data/mvpa_accuracies_D2024-10-09_T17-06-55.npy')
-    # plot_accuracies(path='/Users/joyet/Documents/Documents - Guillaume’s MacBook Pro/UniBasel/MSc_Data_Science/Master Thesis/Code/eeg-master-thesis/mvpa/results/data/mvpa_accuracies_D2024-10-09_T18-06-23.npy')
